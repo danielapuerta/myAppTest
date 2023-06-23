91d1193908c2880a21ad4b35c8fa52bf15e7498a
danielapuerta <danielapuerta151@gmail.com>
2023-06-23 21:10:52 +0100

    testing POST with jqueryAJAX part 1

--- routes/index.js
+++ routes/index.js
@@ -12,13 +12,17 @@ router.get('/test', function(req, res, next) {
 });
 
 router.post('/test', function(req, res, next) {
-  console.log(req.body.username)
-  res.send("200")
+  console.log(req.body)
+  res.status(200).send("Data received!")
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login', { title: 'SeniorCare' });
 });
 
+router.post('/login', function(req, res, next) {
+  console.log(req.body)
+  res.status(200).send("Data received!")
+});
 
 module.exports = router;
