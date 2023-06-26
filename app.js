d43c1f6927374d7204d8e91f050b4d9055fc2d52
danielapuerta <danielapuerta151@gmail.com>
2023-06-26 12:31:03 +0100

    creating knex file. Contains db config for development, stagin and prod enviro

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser')
+var knex = require('knex')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(knex());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
